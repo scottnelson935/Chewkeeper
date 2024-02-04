f7f1eb87d729f7741f843235e1761f88e1d29142
scottnelson935 <77465002+scottnelson935@users.noreply.github.com>
2024-02-04 16:04:16 -0600

    20240204_initialUI

--- js/UI.js
+++ js/UI.js
@@ -1,14 +1,36 @@
+function intro() {
+  background(0);
+  textFont("Oswald");
+  textSize(windowWidth * 0.09);
+  fill(10, 40, 50, 200);
+  fill("white");
+  if (windowWidth < windowHeight) {
+    text("Touch to begin", windowWidth / 2, windowHeight / 2.5);
+  } else if (windowWidth > windowHeight) {
+    text("Touch to begin", windowWidth / 2, windowHeight / 2);
+  }
+}
+
 function title() {
   //title rect
   fill(80, 90, 100, 80);
-  rect(windowWidth / 2, windowHeight / 2, 300, 60);
+  if (windowWidth < windowHeight) {
+    rect(windowWidth / 2, windowHeight / 2.5 - 5, windowWidth * 0.5, windowWidth * 0.15);
+  } else if (windowWidth > windowHeight) {
+    rect(windowWidth / 2, windowHeight / 2 - 5, windowWidth * 0.5, windowWidth * 0.15);
+  }
 
   //title text
   push();
   textFont("Oswald");
-  textSize(38);
+  textSize(windowWidth * 0.09);
   fill(10, 40, 50, 200);
-  text("CHEWKEEPER", windowWidth / 2, windowHeight / 2 + 3);
+
+  if (windowWidth < windowHeight) {
+    text("CHEWKEEPER", windowWidth / 2, windowHeight / 2.5);
+  } else if (windowWidth > windowHeight) {
+    text("CHEWKEEPER", windowWidth / 2, windowHeight / 2);
+  }
   pop();
 }
 
@@ -31,27 +53,59 @@ function instructions() {
 }
 
 function feeding() {
-  let tBoxAlpha = 120;
-  let tBoxAlphaDir = -1;
+  tBoxAlpha += tBoxAlphaDir * 0.2;
 
-  tBoxAlpha += tBoxAlphaDir;
-  
-  if (tBoxAlpha <= 80) {
-    tBoxAlphaDir = 1;
-  } else if (tBoxAlpha >= 200) {
-    tBoxAlphaDir = -1;
+  if (tBoxAlpha <= 80 || tBoxAlpha >= 200) {
+    tBoxAlphaDir *= -1;
   }
-
-  print(tBoxAlpha);
-
-  //tiger image
   push();
   translate(windowWidth / 2, windowHeight / 2);
-  //tiger rect
-  fill(80, 90, 100, 80);
-  rect(0, 0, 210, 120);
   imageMode(CENTER);
-  scale(0.05);
-  image(tigerImg, 0, 0);
+  for (let i = 0; i < animalBoxes.length; i++) {
+    animalBoxes[i].display();
+    animalBoxes[i].isInside();
+  }
   pop();
+}
+
+class animalBox {
+  constructor(x, y, width, height, image, sound, scale, name) {
+    this.name = name;
+    this.x = x; // x-coordinate of the object
+    this.y = y; // y-coordinate of the object
+    this.width = width; // width of the object
+    this.height = height; // height of the object
+    this.image = image; // image to display
+    this.sound = sound; // sound to play when clicked
+    this.clicked = false; // flag to track if the object is clicked
+    this.scale = scale;
+  }
+
+  // Function to check if a point (mouse) is inside the object's bounds
+  isInside(mouseX, mouseY) {
+    fill("black");
+    text(mouseX + mouseY, windowWidth - 200, windowHeight = 100);
+    return (
+      mouseX >= this.x &&
+      mouseX <= this.x + this.width &&
+      mouseY >= this.y &&
+      mouseY <= this.y + this.height
+    );
+  }
+
+  // Function to display the object
+  display() {
+    fill(80, 90, 100, tBoxAlpha);
+    rect(this.x, this.y, this.width, this.height);
+    image(this.image, this.x, this.y, this.width, this.height);
+  }
+
+  // Function to handle click event
+  handleClick() {
+    if (this.isInside(mouseX, mouseY) && this.clicked === false) {
+      // Check if the mouse is inside the object
+      this.sound.start(); // Play the associated sound
+      this.clicked = true; // Set the clicked flag to true
+    }
+  }
 }
--- sketch.js
+++ sketch.js
@@ -12,28 +12,66 @@ let cloudAlpha = 255;
 let cloudAlpha2 = 0;
 let cloudAlphaDirection = -1;
 
+let tBoxAlpha = 120;
+let tBoxAlphaDir = -1;
+
 let drops = [];
 
 let bgBuffer;
 
 let state = 0;
+let prevMouseState = false; // Track previous mouse state
+let currentMouseState = false; // Track current mouse state
+let lastClickTime = 0; // Track the time of the last click
+const clickCooldown = 500; // Set a cooldown time in milliseconds
 
 let tigerImg;
+let giraffeImg;
 let rain;
 let tigerSound;
+let giraffeSound;
+
+let tigerObj;
+let giraffeObj;
+
+let animalBoxes = [];
+
+let audioStarted = false;
+
+let rainStart = false;
+
+let textSizeFactor = 0.09;
+
+let state0 = true;
+let state1 = false;
+let state2 = false;
+let state3 = false;
+
+let lpf;
 
 function preload() {
   for (let i = 1; i <= numClouds; i++) {
     clouds.push(loadImage(`images/clouds${i}.jpeg`));
   }
 
-  rain = new Tone.Player("sounds/sample_rain.ogg").toDestination();
+  lpf = new Tone.Filter(700, "lowpass", -12).toDestination();
+
+  rain = new Tone.Player("sounds/sample_rain.ogg");
+  rain.loop = true;
+  rain.connect(lpf);
+
 
   tigerSound = new Tone.Player(
     "https://cdn.freesound.org/previews/194/194943_1160789-lq.mp3"
   ).toDestination();
 
+  giraffeSound = new Tone.Player(
+    "https://cdn.freesound.org/previews/35/35143_328279-lq.mp3"
+  ).toDestination();
+
+
   tigerImg = loadImage("images/tiger.png");
+  giraffeImg = loadImage("images/giraffe.png");
 }
 
 function setup() {
@@ -45,9 +83,11 @@ function setup() {
   centerX = canvasWidth / 2;
   centerY = canvasHeight / 2;
 
-  if (rain) {
-    rain.start();
-  }
+  tigerObj = new animalBox(-150, 0, 210, 210, tigerImg, tigerSound, 0.05, "tiger");
+  giraffeObj = new animalBox(150, 0, 210, 210, giraffeImg, giraffeSound, 0.05, "giraffe");
+
+  animalBoxes.push(tigerObj);
+  animalBoxes.push(giraffeObj);
 
   bgBuffer = createGraphics(canvasWidth, canvasHeight);
 
@@ -59,26 +99,81 @@ function setup() {
 function draw() {
   bg();
 
-  //title state
+  // print(state);
   if (state === 0) {
+    intro();
+  }
+
+  //title state
+  if (state === 1) {
     title();
+    if (rainStart === false) {
+      rain.start();
+      rainStart = true;
+    }
+    // print(rainStart);
   }
 
   //instructions state
-  if (state === 1) {
+  if (state === 2) {
     instructions();
   }
 
   //feeding state
-  if (state === 2) {
+  if (state === 3) {
     feeding();
   }
-  // print(state);
 }
 
 function keyPressed() {
   if (keyCode === 32) {
     state += 1;
+  }
+}
+
+function mousePressed() {
+  currentMouseState = true; // Update current mouse state
+
+  // Check if the mouse was not pressed in the previous frame
+  if (!prevMouseState) {
+    const currentTime = millis(); // Get current time
+
+    // Check if the time since the last click is greater than the cooldown period
+    if (currentTime - lastClickTime > clickCooldown) {
+      if (state === 0) {
+        startAudioContext();
+      }
+
+      // Add a condition to prevent state increment in the fourth state (state === 3)
+      if (state !== 3) {
+        state = (state + 1) % 4; // Cycle through states 0, 1, 2, and 3
+      } lastClickTime = currentTime; // Update the last click time
+
+      if (state === 3) {
+        for (let i = 0; i < animalBoxes.length; i++) {
+          animalBoxes[i].handleClick();
+          if (animalBoxes[i].clicked) {
+            print(animalBoxes[i].name + "clicked!");
+          }
+        }
+      }
+    }
+  }
+
+  prevMouseState = currentMouseState; // Update previous mouse state
+}
+
+
+function mouseReleased() {
+  currentMouseState = false; // Update current mouse state
+  prevMouseState = currentMouseState; // Update previous mouse state
+}
+
+function startAudioContext() {
+  if (!audioStarted) {
+    Tone.start();
+    audioStarted = true;
+    print("Tone started!");
   }
 }
 
